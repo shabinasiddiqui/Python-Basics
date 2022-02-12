7a147326bed67a6b298b1ab924de8b3e222c2606
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-02-12 15:19:04 +0530

    Corner case

--- SumOfSqrOfNnaturalNumbers.py
+++ SumOfSqrOfNnaturalNumbers.py
@@ -4,6 +4,8 @@ def Sum(n):
         for i in range(1,n+1):
             sum=sum+(i*i)
         return sum
+    else:
+        print(f"Given number {n} is not a natural number: ")
         
         
 n = int(input("Enter a number: "))
