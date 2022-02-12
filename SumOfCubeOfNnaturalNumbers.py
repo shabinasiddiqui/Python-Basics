1f77e73898b7eb107767cebe9d8f3210035a0cff
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-02-12 15:25:15 +0530

    Sum Of Cube Of N Natural Number

--- SumOfCubeOfNnaturalNumbers.py
+++ SumOfCubeOfNnaturalNumbers.py
@@ -2,11 +2,11 @@ def Sum(n):
     sum=0
     if n>=1:
         for i in range(1,n+1):
-            sum=sum+(i*i)
+            sum+=(i**3)
         return sum
     else:
         print(f"Given number {n} is not a natural number: ")
         
         
 n = int(input("Enter a number: "))
-print(f"Sum of square of first {n} natural numbers: {Sum(n)}")
+print(f"Sum of cube of first {n} natural numbers: {Sum(n)}")
