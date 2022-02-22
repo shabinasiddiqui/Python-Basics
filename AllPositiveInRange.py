2d2bd33247058325d07905baaa6d3daed271d7dd
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-02-22 20:58:05 +0530

    All Positive In The Given Range

---
+++ AllPositiveInRange.py
@@ -0,0 +1,12 @@
+def Positive(start, end):
+    new_list=[]
+    for i in range(start,end+1):
+        if start>0:  
+            new_list.append(start)
+        start+=1
+    print(f"Odd numbers in the given Range are: {new_list}") 
+    
+start = int (input("Enter the starting value of range: "))
+end = int (input("Enter the ending value of range: "))
+
+Positive(start,end)
