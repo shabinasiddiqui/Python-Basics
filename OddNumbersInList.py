4418be66ffc5c8d437dcc75f4821792652cf2226
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-02-22 20:22:59 +0530

    Print Odd Numbers in a given List

--- OddNumbersInList.py
+++ OddNumbersInList.py
@@ -1,9 +1,9 @@
-def IsEven(list, new_list): 
+def IsOdd(list, new_list): 
     for i in range(0,len(list)):
         if list[i]%2==1:  
             new_list.append(list[i])
     
 new_list=[]
 list = [ 4, 6, 3, 5, 2, 1, 2, 7, 5, 64, 14] 
-IsEven(list, new_list)
+IsOdd(list, new_list)
 print(f"Odd numbers in the fiven string are: {new_list}") 
