67bf8e223b59ae38aee431c9b4c9ef8ab6a92796
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-02-22 21:02:14 +0530

    All negative in the given range

---
+++ AllNegativeInRange.py
@@ -0,0 +1,12 @@
+def Negative(start, end):
+    new_list=[]
+    for i in range(start,end+1):
+        if start<0:  
+            new_list.append(start)
+        start+=1
+    print(f"All negative numbers in the given Range are: {new_list}") 
+    
+start = int (input("Enter the starting value of range: "))
+end = int (input("Enter the ending value of range: "))
+
+Negative(start,end)
