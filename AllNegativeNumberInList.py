fac3d062413ff6b56b5b852bad3aab3c3ee4587c
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-02-22 20:53:24 +0530

    program to print negative numbers in a list

---
+++ AllNegativeNumberInList.py
@@ -0,0 +1,10 @@
+def NegativeList(list):
+    new_list=[]
+    for i in range(len(list)):
+        if list[i]<0:  
+            new_list.append(list[i])
+        
+    print(f"Negative numbers in the given List are: {new_list}") 
+
+list = [ 4, -6, 3, 5, -2, -1, 2, 7, 5, -64, 14] 
+NegativeList(list)
