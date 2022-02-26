2a32420f7bc4e50ab0745fe5a43500d1a3772a5a
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-02-26 10:06:20 +0530

    program to check whether the string is Symmetrical

---
+++ IsStringSymmetric.py
@@ -0,0 +1,31 @@
+def Symmetric(str):                   #a k a k k a    a k a a k
+                                      #0 1 2 3 4 5
+                                      
+  low=0
+  flag=0
+  n =len(str)
+  if n%2:
+    mid = n//2 +1
+  else:
+    mid = n//2
+      
+  while (low<mid and mid<n):
+    if str[low]== str[mid]:
+      low+=1
+      mid+=1
+    else:
+      flag=1
+      break
+    
+  if flag==0:
+    print("Entered string is Symmetric")
+  else:
+    print("Entered string is not Symmetric")
+
+
+#main function
+str = input("Enter string to check whether its Symmetric or not: ")
+Symmetric(str)
+  
+
+  
